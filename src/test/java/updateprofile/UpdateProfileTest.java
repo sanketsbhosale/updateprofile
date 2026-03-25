cd764cbc1bc06309d2e1af3e775bd484fe335aba
Sankeypython <53823909+sanketsbhosale@users.noreply.github.com>
2026-03-25 11:20:51 +0530

    Update UpdateProfileTest.java

--- src/test/java/updateprofile/UpdateProfileTest.java
+++ src/test/java/updateprofile/UpdateProfileTest.java
@@ -1,5 +1,6 @@
 package updateprofile;
 
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
