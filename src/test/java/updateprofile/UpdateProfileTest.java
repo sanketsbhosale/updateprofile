ca8ca3b6377b4d1f630ab2d1823bbe45dfbea756
sanketsbhosale <sanketsbhosale@outlook.com>
2026-04-02 03:35:56 +0530

    Updated 8

--- src/test/java/updateprofile/UpdateProfileTest.java
+++ src/test/java/updateprofile/UpdateProfileTest.java
@@ -162,7 +162,7 @@ public class UpdateProfileTest {
 
         WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(NAME_INPUT));
         String currentName = normalizeWhitespace(nameInput.getAttribute("value"));
-        String updatedName = buildUpdatedName(currentName);
+        String updatedName = buildUpdatedName(currentName) + "test";
 
         clearAndType(nameInput, updatedName);
         logger.info("Updated basic details name field to trigger a profile refresh");
